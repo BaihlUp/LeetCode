b737c09c56f07e22498930d18df291124942534b
白浩良 <baihaoliang@inspur.com>
2022-08-11 09:44:42 +0800

    add atoi

---
+++ 8.字符串转换整数-atoi.go
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode.cn id=8 lang=golang
+ *
+ * [8] 字符串转换整数 (atoi)
+ */
+
+// @lc code=start
+func myAtoi(s string) int {
+	index, sign, length := 0, 1, len(s)
+	const MinInt32, MaxInt32 = -1 << 31, 1<<31 - 1
+	res := 0
+	if length <= 0 {
+		return 0
+	}
+	for ; index < length && s[index] == ' '; index++ {
+	}
+	if index >= length {
+		return 0
+	}
+	switch s[index] {
+	case '+':
+		index++
+	case '-':
+		index++
+		sign = -1
+	}
+
+	for ; index < length; index++ {
+		if int(s[index]-'0') < 0 || int(s[index]-'0') > 9 {
+			break
+		}
+		res = res*10 + int(s[index]-'0')
+		if sign*res < MinInt32 {
+			return MinInt32
+		}
+		if sign*res > MaxInt32 {
+			return MaxInt32
+		}
+	}
+	return res * sign
+}
+
+// @lc code=end
