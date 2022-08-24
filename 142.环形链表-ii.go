7d84a82cb0307698fa7d97053206c4d006612fbd
白浩良 <2677443264@qq.com>
2022-08-24 15:58:56 +0800

    add 142、148

---
+++ 142.环形链表-ii.go
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=142 lang=golang
+ *
+ * [142] 环形链表 II
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func detectCycle(head *ListNode) *ListNode {
+	low, fast := head, head
+	for fast != nil && fast.Next != nil {
+		low = low.Next
+		fast = fast.Next.Next
+		if low == fast {
+			break
+		}
+	}
+	if fast == nil || fast.Next == nil {
+		return nil
+	}
+	fast = head
+	for low != fast {
+		low = low.Next
+		fast = fast.Next
+	}
+	return low
+}
+
+// @lc code=end
+
