32126f5439ef959a00f22b6249b7ca78703a8856
baihlUp <2677443264@qq.com>
2022-10-18 14:58:02 +0800

    modify 7

--- 7.整数反转.go
+++ 7.整数反转.go
@@ -10,11 +10,11 @@ func reverse(x int) int {
 	fmt.Println(reflect.TypeOf(res))
 
 	for x != 0 {
+		if res < math.MinInt32/10 || res > math.MaxInt32/10 {
+			return 0
+		}
 		res = res*10 + x%10
 		x = x / 10
-	}
-	if res > 1<<31-1 || res < -(1<<31) {
-		return 0
 	}
 	return res
 }
