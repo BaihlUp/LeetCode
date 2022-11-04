62ca68c637a0bcdb454b062ec7144954cac46504
baihlUp <2677443264@qq.com>
2022-11-04 17:50:39 +0800

    add 189

---
+++ 189.轮转数组.go
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=189 lang=golang
+ *
+ * [189] 轮转数组
+ */
+
+// @lc code=start
+func rotate(nums []int, k int) {
+	k = k % len(nums)
+	revers(nums)
+	revers(nums[:k])
+	revers(nums[k:])
+}
+
+func revers(a []int) {
+	i, j := 0, len(a)-1
+	for i < j {
+		a[i], a[j] = a[j], a[i]
+		i++
+		j--
+	}
+}
+
+// @lc code=end
+
