6580fc52a5246d8afd9af1941ec78faa04f6750b
baihlUp <2677443264@qq.com>
2022-11-29 22:39:20 +0800

    add 240

---
+++ 240.搜索二维矩阵-ii.go
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=240 lang=golang
+ *
+ * [240] 搜索二维矩阵 II
+ */
+
+// @lc code=start
+func searchMatrix(matrix [][]int, target int) bool {
+	if len(matrix) == 0 {
+		return false
+	}
+	row, col := 0, len(matrix[0])-1
+	for row < len(matrix) && col >= 0 {
+		if matrix[row][col] == target {
+			return true
+		}
+		if matrix[row][col] > target {
+			col--
+		} else {
+			row++
+		}
+	}
+	return false
+}
+
+// @lc code=end
+
