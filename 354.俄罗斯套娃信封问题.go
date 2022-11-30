563402abdb9c68894f0e9eeca4b49cb2c6eda9d4
baihlUp <2677443264@qq.com>
2022-11-30 11:36:23 +0800

    add 354

---
+++ 354.俄罗斯套娃信封问题.go
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode.cn id=354 lang=golang
+ *
+ * [354] 俄罗斯套娃信封问题
+ */
+
+// @lc code=start
+func maxEnvelopes(envelopes [][]int) int {
+	sort.Slice(envelopes, func(i, j int) bool {
+		if envelopes[i][0] == envelopes[j][0] {
+			return envelopes[i][1] > envelopes[j][1]
+		}
+		return envelopes[i][0] < envelopes[j][0]
+	})
+
+	res := []int{}
+	for i := 0; i < len(envelopes); i++ {
+		if idx := sort.SearchInts(res, envelopes[i][1]); idx >= len(res) {
+			res = append(res, envelopes[i][1])
+		} else {
+			res[idx] = envelopes[i][1]
+		}
+	}
+	return len(res)
+}
+
+func maxEnvelopes1(envelopes [][]int) int {
+	sort.Slice(envelopes, func(i, j int) bool {
+		if envelopes[i][0] == envelopes[j][0] {
+			return envelopes[i][1] > envelopes[j][1]
+		}
+		return envelopes[i][0] < envelopes[j][0]
+	})
+
+	res, dp := 0, make([]int, len(envelopes))
+	for i := 0; i < len(envelopes); i++ {
+		for j := 0; j < i; j++ {
+			if envelopes[i][1] > envelopes[j][1] {
+				dp[i] = max(dp[i], dp[j])
+			}
+		}
+		dp[i] += 1
+		res = max(res, dp[i])
+	}
+	return res
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+// @lc code=end
+
