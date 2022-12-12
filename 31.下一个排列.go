2cd0cb52cb0e027d66bc41ca5b15efe715d416a6
baihlUp <2677443264@qq.com>
2022-12-12 10:40:19 +0800

    add 31

--- 31.下一个排列.go
+++ 31.下一个排列.go
@@ -5,8 +5,29 @@
  */
 
 // @lc code=start
-func nextPermutation(nums []int)  {
+func nextPermutation(nums []int) {
+	size, pivot := len(nums), len(nums)-2
+	for pivot >= 0 && nums[pivot] >= nums[pivot+1] {
+		pivot--
+	}
+	if pivot >= 0 {
+		i := size - 1
+		for i >= 0 && nums[i] <= nums[pivot] {
+			i--
+		}
+		nums[i], nums[pivot] = nums[pivot], nums[i]
+	}
 
+	reverse(nums, pivot+1, size-1)
 }
+
+func reverse(nums []int, start, end int) {
+	for start <= end {
+		nums[start], nums[end] = nums[end], nums[start]
+		start++
+		end--
+	}
+}
+
 // @lc code=end
 
