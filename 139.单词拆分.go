4d8c2fba44f0def34fb26ad49e93b004f10fed77
baihlUp <2677443264@qq.com>
2023-02-21 09:22:30 +0800

    add 139

---
+++ 139.单词拆分.go
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=139 lang=golang
+ *
+ * [139] 单词拆分
+ */
+
+// @lc code=start
+//dp[i] 标识前 i 个字符可以被 wordDict 拼接出
+func wordBreak(s string, wordDict []string) bool {
+	wordDictSet := make(map[string]bool)
+	for _, word := range wordDict {
+		wordDictSet[word] = true
+	}
+	dp := make([]bool, len(s)+1)
+	dp[0] = true
+	for i := 1; i <= len(s); i++ {
+		for j := 0; j < len(s); j++ {
+			if dp[j] && wordDictSet[s[j:i]] {
+				dp[i] = true
+				break
+			}
+		}
+	}
+	return dp[len(s)]
+}
+
+// @lc code=end
+
