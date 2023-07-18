115e2405322181b69485ec8557a7bf27f8f916b3
baihlUp <2677443264@qq.com>
2023-07-18 18:00:56 +0800

    add 703

---
+++ C++/703.数据流中的第-k-大元素.cpp
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=703 lang=cpp
+ *
+ * [703] 数据流中的第 K 大元素
+ */
+
+// @lc code=start
+class KthLargest {
+public:
+    priority_queue<int, vector<int>, greater<int>> ique;
+    int k;
+    KthLargest(int k, vector<int>& nums) {
+        this->k = k;
+        for(auto val : nums) {
+            add(val);
+        }
+    }
+    
+    int add(int val) {
+        ique.push(val);
+        if(ique.size() > k) {
+            ique.pop();
+        }
+        return ique.top();
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
+// @lc code=end
+
