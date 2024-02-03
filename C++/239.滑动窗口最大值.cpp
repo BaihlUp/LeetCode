eaf4121d59ec99524bb924c5e6253e7e06af9c71
BaihlUp <2677443264@qq.com>
2024-02-03 10:31:01 +0800

    add test

--- C++/239.滑动窗口最大值.cpp
+++ C++/239.滑动窗口最大值.cpp
@@ -32,3 +32,43 @@ public:
 };
 // @lc code=end
 
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    Solution sol;
+
+    // 测试示例 1
+    vector<int> nums1 = {1, 3, -1, -3, 5, 3, 6, 7};
+    int k1 = 3;
+    vector<int> result1 = sol.maxSlidingWindow(nums1, k1);
+    cout << "Result 1: ";
+    for (int num : result1) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    // 测试示例 2
+    vector<int> nums2 = {1, -1};
+    int k2 = 1;
+    vector<int> result2 = sol.maxSlidingWindow(nums2, k2);
+    cout << "Result 2: ";
+    for (int num : result2) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    // 测试示例 3
+    vector<int> nums3 = {9, 11};
+    int k3 = 2;
+    vector<int> result3 = sol.maxSlidingWindow(nums3, k3);
+    cout << "Result 3: ";
+    for (int num : result3) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
--- Go/239.滑动窗口最大值.go
+++ Go/239.滑动窗口最大值.go
@@ -30,3 +30,22 @@ func maxSlidingWindow(nums []int, k int) []int {
 
 // @lc code=end
 
+func main() {
+    // 测试示例 1
+    nums1 := []int{1, 3, -1, -3, 5, 3, 6, 7}
+    k1 := 3
+    result1 := maxSlidingWindow(nums1, k1)
+    fmt.Println("Result 1:", result1)
+
+    // 测试示例 2
+    nums2 := []int{1, -1}
+    k2 := 1
+    result2 := maxSlidingWindow(nums2, k2)
+    fmt.Println("Result 2:", result2)
+
+    // 测试示例 3
+    nums3 := []int{9, 11}
+    k3 := 2
+    result3 := maxSlidingWindow(nums3, k3)
+    fmt.Println("Result 3:", result3)
+}
