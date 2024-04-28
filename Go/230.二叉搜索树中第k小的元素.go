ca67a6b55da3ae5d1941bee7a369aa12253fbf80
BaihlUp <2677443264@qq.com>
2024-04-28 14:32:29 +0800

    add 230,543

---
+++ Go/230.二叉搜索树中第k小的元素.go
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=230 lang=golang
+ *
+ * [230] 二叉搜索树中第K小的元素
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func kthSmallest(root *TreeNode, k int) int {
+	res := make([]int, 0)
+	var inorder func(*TreeNode)
+	inorder = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+		inorder(node.Left)
+		res = append(res, node.Val)
+		inorder(node.Right)
+	}
+	inorder(root)
+	return res[k-1]
+}
+
+// @lc code=end
+
---
+++ Go/543.二叉树的直径.go
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=543 lang=golang
+ *
+ * [543] 二叉树的直径
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func diameterOfBinaryTree(root *TreeNode) int {
+	maxPath := 0
+	var dfs func(node *TreeNode) int
+	dfs = func(node *TreeNode) int {
+		if node == nil {
+			return 0
+		}
+		left := dfs(node.Left)
+		right := dfs(node.Right)
+		maxPath = max(maxPath, left+right)
+		return max(left, right) + 1
+	}
+	dfs(root)
+	return maxPath
+}
+
+// @lc code=end
+
