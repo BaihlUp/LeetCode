a32d77f55eb399475b18949761206fa9844467c5
BaihlUp <2677443264@qq.com>
2024-04-29 21:26:22 +0800

    add 215

--- Go/215.数组中的第k个最大元素.go
+++ Go/215.数组中的第k个最大元素.go
@@ -6,7 +6,30 @@
 
 // @lc code=start
 func findKthLargest(nums []int, k int) int {
-
+	return quickSelect(nums, 0, len(nums)-1, len(nums)-k)
 }
+func quickSelect(nums []int, left, right, k int) int {
+	if left == right {
+		return nums[left]
+	}
+	pivot := nums[left]
+	i := left - 1
+	j := right + 1
+	for i < j {
+		for i++; nums[i] < pivot; i++ {
+		}
+		for j--; nums[j] > pivot; j-- {
+		}
+		if i < j {
+			nums[i], nums[j] = nums[j], nums[i]
+		}
+	}
+	if k <= j {
+		return quickSelect(nums, left, j, k)
+	} else {
+		return quickSelect(nums, j+1, right, k)
+	}
+}
+
 // @lc code=end
 
